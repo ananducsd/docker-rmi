91e614b3b8645e4e1badbb43424f02888ad06af1
Siddhant Arya <SiddhantArya@github.com>
2016-05-24 00:42:48 -0700

    adding storage server

--- dfs/src/storage/StorageServer.java
+++ dfs/src/storage/StorageServer.java
@@ -16,6 +16,11 @@ import naming.*;
  */
 public class StorageServer implements Storage, Command
 {
+
+    File root;
+    Skeleton<Storage> storage;
+    Skeleton<Command> command; 
+
     /** Creates a storage server, given a directory on the local filesystem, and
         ports to use for the client and command interfaces.
 
@@ -33,7 +38,26 @@ public class StorageServer implements Storage, Command
     */
     public StorageServer(File root, int client_port, int command_port)
     {
-        throw new UnsupportedOperationException("not implemented");
+        InetSocketAddress addrStorage, addrCommand;
+        if (root == null) {
+            throw new NullPointerException("Root is null");
+        }
+        
+        this.root = root;
+        if(client_port > 0) {
+            addrStorage = new InetSocketAddress(client_port);
+            storage = new Skeleton<Storage>(Storage.class, this, addrStorage);
+        } else {
+            clientSkeleton = new Skeleton<Storage>(Storage.class, this);
+        }
+
+        if(command_port > 0) {
+            addrCommand = new InetSocketAddress(command_port);
+            command = new Skeleton<Command>(Command.class, this, addrCommand);
+        } else {
+            commandSkeleton = new Skeleton<Command>(Command.class, this);
+        }
+
     }
 
     /** Creats a storage server, given a directory on the local filesystem.
@@ -49,7 +73,7 @@ public class StorageServer implements Storage, Command
      */
     public StorageServer(File root)
     {
-        throw new UnsupportedOperationException("not implemented");
+        this(root, 0, 0);
     }
 
     /** Starts the storage server and registers it with the given naming
@@ -75,7 +99,31 @@ public class StorageServer implements Storage, Command
     public synchronized void start(String hostname, Registration naming_server)
         throws RMIException, UnknownHostException, FileNotFoundException
     {
-        throw new UnsupportedOperationException("not implemented");
+        if( !root.isDirectory() || !root.exists() ) {
+            throw new FileNotFoundException("Server root not found or is not a directory");
+        }
+        storage.start();
+        command.start();
+
+        Path[] serverFiles = naming_server.register(
+                Stub.create(Storage.class, storage, hostname),
+                Stub.create(Command.class, command, hostname),
+                Path.list(root));
+
+        // Storage Server startup  deletes all duplicate files on server.
+        for(Path p : serverFiles) {
+            p.toFile(root).delete();
+            File parent = new File(p.toFile(root).getParent()));
+            
+            while(!parent.equals(root)) {
+                if(parent.list().length == 0) {
+                    parent.delete();
+                } else {
+                    break;
+                }
+                parent = new File(parent.getParent());
+            }
+        }        
     }
 
     /** Stops the storage server.
@@ -85,7 +133,8 @@ public class StorageServer implements Storage, Command
      */
     public void stop()
     {
-        throw new UnsupportedOperationException("not implemented");
+        storage.stop();
+        command.stop();
     }
 
     /** Called when the storage server has shut down.
@@ -101,40 +150,151 @@ public class StorageServer implements Storage, Command
     @Override
     public synchronized long size(Path file) throws FileNotFoundException
     {
-        throw new UnsupportedOperationException("not implemented");
+        File f = file.toFile(root);
+        if (!f.exists() || f.isDirectory())
+            throw new FileNotFoundException("File not found or is a directory");
+        return f.length();    
     }
 
     @Override
     public synchronized byte[] read(Path file, long offset, int length)
         throws FileNotFoundException, IOException
     {
-        throw new UnsupportedOperationException("not implemented");
+        File f = file.toFile(root);
+        if(!f.exists() || f.isDirectory()) {
+            throw new FileNotFoundException("File not found or is a directory");
+        }
+        if((offset < 0) 
+            || (length < 0)
+            || offset > Integer.MAX_VALUE  
+            || (offset + length) > f.length()
+            ) {
+            throw new IndexOutOfBoundsException("Illegal Memory Access");
+        }
+        // reads from the file using FileInputStream and returns the content
+        InputStream reader = new FileInputStream(f);
+        byte[] output = new byte[length];
+        reader.read(output, (int) offset, length);
+        return output;    
     }
 
     @Override
     public synchronized void write(Path file, long offset, byte[] data)
         throws FileNotFoundException, IOException
     {
-        throw new UnsupportedOperationException("not implemented");
+        File f = file.toFile(root);
+        if(!f.exists() || f.isDirectory()) {
+            throw new FileNotFoundException("File not found or is a directory");
+        }
+        if(offset < 0) {
+            throw new IndexOutOfBoundsException("The offset is negative");
+        }
+
+        InputStream reader = new FileInputStream(f);
+        FileOutputStream writer = new FileOutputStream(f);
+
+        long bufferSize = Math.min(offset, f.length());
+        byte[] buffer = new byte[(int) bufferSize];
+
+        reader.read(buffer);
+        writer.write(buffer, 0, (int) bufferSize);
+
+        long fill = offset - f.length();
+        if(fill > 0) {
+            for(int i = 0; i < (int) fill; i ++) {
+                writer.write(0);
+            }
+        }
+        writer.write(data);
     }
 
     // The following methods are documented in Command.java.
     @Override
     public synchronized boolean create(Path file)
     {
-        throw new UnsupportedOperationException("not implemented");
+        File f = file.toFile(root);
+        if(file == null) {
+            throw new NullPointerException("File to be created can't be null");
+        }
+
+        if (file.isRoot()) {
+            return false;
+        }
+
+        File parent = file.parent().toFile(root);
+        parent.mkdirs();
+
+        try {
+            return f.createNewFile();
+        } catch (IOException e) {
+            return false;
+        }
     }
 
     @Override
     public synchronized boolean delete(Path path)
     {
-        throw new UnsupportedOperationException("not implemented");
+        File f = path.toFile(root);
+        if(path.isRoot() || !f.exists() ) {
+            return false;
+        }
+
+        if(f.isDirectory()) {
+            return deleteDirectory(f);
+        } else {
+            return f.delete();
+        }        
     }
+
+    private boolean deleteDirectory(File f) {
+        if(f.isDirectory()) {
+            for(File item : f.listFiles()) {
+                if(!deleteDirectory(item)) {
+                    return false;
+                }
+            }
+        }
+        return f.delete();
+    }
+
 
     @Override
     public synchronized boolean copy(Path file, Storage server)
         throws RMIException, FileNotFoundException, IOException
     {
-        throw new UnsupportedOperationException("not implemented");
+       if (file == null || server == null)
+            throw new NullPointerException("Illegal / Null arguments provided");
+
+        File f = file.toFile(root);
+        if (f.exists()) {
+            delete(file);
+        }
+
+        if (!create(file)) {
+            throw new IOException("Error creating file");
+        }
+
+        long size = server.size(file);
+
+        int buffsize = 8192; 
+        byte[] buffer = new byte[buffsize];
+
+        for (long i = 0; i * buffsize <= size; i++) {
+            long done = i * buffsize;
+            if ( (i+1) * buffsize > size) {
+                buffer = server.read(file, 
+                                done, 
+                                (int) (size - done));
+                write(file,
+                    done,
+                    Arrays.copyOfRange(buffer, 
+                                        0, 
+                                        (int) (size - done)));
+            } else {
+                buffer = server.read(file, (int) done, buffer.length);
+                write(file, (int) done, buffer);
+            }
+        }
+        return true;        
     }
 }
